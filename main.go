9b9004068b7b37c1d451b454de358c1e8d8909d5
Gio d'Amelio <giodamelio@gmail.com>
2022-03-07 05:07:19 +0000

    Print the IP addresses from all the docker containers

---
+++ main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/docker/docker/api/types"
+	"github.com/docker/docker/client"
+)
+
+func main() {
+	cli, err := client.NewClientWithOpts(client.FromEnv)
+	if err != nil {
+		panic(err)
+	}
+
+	containers, err := cli.ContainerList(context.Background(), types.ContainerListOptions{})
+	if err != nil {
+		panic(err)
+	}
+
+	for _, container := range containers {
+		fmt.Printf("%s:\n", container.Names[0])
+		fmt.Printf("%#v\n", container.NetworkSettings.Networks["shared"].IPAddress)
+	}
+}
